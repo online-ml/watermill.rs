25178c41c20feb5e7d7a263c54ea5c15a2436fd4
Adil Zouitine <adilzouitinegm@gmail.com>
2022-07-11 17:43:23 +0200

    Add running sum

---
+++ src/sum.rs
@@ -0,0 +1,32 @@
+use num::{Float, FromPrimitive};
+use std::ops::{AddAssign, SubAssign};
+/// Running sum.
+/// # Examples
+/// ```
+/// use online_statistics::sum::Sum;
+/// let mut running_sum: Sum<f64> = Sum::new();
+/// for i in 1..10{
+///     running_sum.update(i as f64);
+/// }
+/// assert_eq!(running_sum.get(), 45.0);
+/// ```
+///
+#[derive(Copy, Clone, Default, Debug)]
+pub struct Sum<F: Float + FromPrimitive + AddAssign + SubAssign> {
+    pub sum: F,
+}
+
+impl<F: Float + FromPrimitive + AddAssign + SubAssign> Sum<F> {
+    pub fn new() -> Self {
+        Self {
+            sum: F::from_f64(0.0).unwrap(),
+        }
+    }
+    #[warn(unused_variables)]
+    pub fn update(&mut self, x: F) {
+        self.sum += x;
+    }
+    pub fn get(&mut self) -> F {
+        self.sum
+    }
+}
