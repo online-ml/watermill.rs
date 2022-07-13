c302faf86132b2e92f545c91d6bda9e8acc0ed29
Adil Zouitine <adilzouitinegm@gmail.com>
2022-07-13 19:25:46 +0200

    add bivariate stats

--- src/traits.rs
+++ src/traits.rs
@@ -2,5 +2,12 @@ use num::{Float, FromPrimitive};
 use std::ops::{AddAssign, SubAssign};
 pub trait Univariate<F: Float + FromPrimitive + AddAssign + SubAssign> {
     fn update(&mut self, x: F);
+    ///The use of `get` as a `&mut self` method is highly debatable, the only use is for computing
+    /// online quantiles. I haven't found a satisfying solution to make `get` non-mutable.
+    fn get(&mut self) -> F;
+}
+
+pub trait Bivariate<F: Float + FromPrimitive + AddAssign + SubAssign> {
+    fn update(&mut self, x: F, y: F);
     fn get(&mut self) -> F;
 }
