ee580c4eaa4519e59c2be3a89d213a60a222444d
Adil Zouitine <adilzouitinegm@gmail.com>
2022-07-19 16:52:35 +0200

    Add Rollable RollableUnivariate  and change Univariate

--- src/traits.rs
+++ src/traits.rs
@@ -4,10 +4,19 @@ pub trait Univariate<F: Float + FromPrimitive + AddAssign + SubAssign> {
     fn update(&mut self, x: F);
     ///The use of `get` as a `&mut self` method is highly debatable, the only use is for computing
     /// online quantiles. I haven't found a satisfying solution to make `get` non-mutable.
-    fn get(&mut self) -> F;
+    fn get(&self) -> F;
 }
 
 pub trait Bivariate<F: Float + FromPrimitive + AddAssign + SubAssign> {
     fn update(&mut self, x: F, y: F);
-    fn get(&mut self) -> F;
+    fn get(&self) -> F;
+}
+
+pub trait Rollable<F: Float + FromPrimitive + AddAssign + SubAssign> {
+    fn revert(&mut self, x: F) -> Result<(), &'static str>;
+}
+
+pub trait RollableUnivariate<F: Float + FromPrimitive + AddAssign + SubAssign>:
+    Rollable<F> + Univariate<F>
+{
 }
