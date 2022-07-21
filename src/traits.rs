1da8cbe2a20a9494d73f239678ac045fc8dac227
Adil Zouitine <adilzouitinegm@gmail.com>
2022-07-22 00:23:38 +0200

    Remove obsolete comment

--- src/traits.rs
+++ src/traits.rs
@@ -2,8 +2,6 @@ use num::{Float, FromPrimitive};
 use std::ops::{AddAssign, SubAssign};
 pub trait Univariate<F: Float + FromPrimitive + AddAssign + SubAssign> {
     fn update(&mut self, x: F);
-    ///The use of `get` as a `&mut self` method is highly debatable, the only use is for computing
-    /// online quantiles. I haven't found a satisfying solution to make `get` non-mutable.
     fn get(&self) -> F;
 }
 
