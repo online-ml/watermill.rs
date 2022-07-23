e318d0d27c30b5ed4b077ea11a099cc53cda124c
Adil Zouitine <adilzouitinegm@gmail.com>
2022-07-23 11:03:35 +0200

    Add documentation

--- src/iter.rs
+++ src/iter.rs
@@ -42,7 +42,19 @@ where
     }
 }
 
-pub trait IterStatExt: Iterator {
+pub trait IterStatisticsExtend: Iterator {
+    /// Running sum.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3.];
+    /// let vec_true: Vec<f64> = vec![1., 3., 6.];
+    /// for (d, t) in data.into_iter().online_sum().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_sum(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -53,7 +65,18 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
-
+    /// Running mean.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3.];
+    /// let vec_true: Vec<f64> = vec![1., 1.5, 2.];
+    /// for (d, t) in data.into_iter().online_mean().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_mean(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -64,6 +87,18 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running count.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3.];
+    /// let vec_true: Vec<f64> = vec![1., 2., 3.];
+    /// for (d, t) in data.into_iter().online_count().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_count(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -75,6 +110,20 @@ pub trait IterStatExt: Iterator {
         }
     }
 
+    /// Running exponentially weighted mean.
+    /// # Arguments
+    /// * `alpha` - The closer `alpha` is to 1 the more the statistic will adapt to recent values. Default value is `0.5`.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3.];
+    /// let vec_true: Vec<f64> = vec![1., 1.9, 2.89];
+    /// for (d, t) in data.into_iter().online_ewmean(0.9_f64).zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_ewmean(self, alpha: Self::Item) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -85,7 +134,20 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
-
+    /// Running exponentially weighted variance.
+    /// # Arguments
+    /// * `alpha` - The closer `alpha` is to 1 the more the statistic will adapt to recent values. Default value is `0.5`.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3.];
+    /// let vec_true: Vec<f64> = vec![0., 0.0900000000000003, 0.11789999999999878];
+    /// for (d, t) in data.into_iter().online_ewvar(0.9_f64).zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_ewvar(self, alpha: Self::Item) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -96,7 +158,21 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
-
+    /// Running inter quantile range.
+    /// # Arguments
+    /// * `q_inf` - Desired inferior quantile, must be between 0 and 1. Defaults to `0.25`.
+    /// * `q_sup` -  Desired superior quantile, must be between 0 and 1. Defaults to `0.75`.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., 4.];
+    /// let vec_true: Vec<f64> = vec![0., 1., 2., 2.];
+    /// for (d, t) in data.into_iter().online_iqr(0.25_f64, 0.75_f64).zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_iqr(self, q_inf: Self::Item, q_sup: Self::Item) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -107,6 +183,20 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running Kurtosis.
+    /// # Arguments
+    /// * `bias` - If `false`, then the calculations are corrected for statistical bias.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., 4.];
+    /// let vec_true: Vec<f64> = vec![-3., -2., -1.5, -1.200000000000001];
+    /// for (d, t) in data.into_iter().online_kurtosis(false).zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_kurtosis(self, bias: bool) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -117,6 +207,18 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running max.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., 2.];
+    /// let vec_true: Vec<f64> = vec![1., 2., 3., 3.];
+    /// for (d, t) in data.into_iter().online_max().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_max(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -127,7 +229,18 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
-
+    /// Running absolute max.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., -4.];
+    /// let vec_true: Vec<f64> = vec![1., 2., 3., 4.];
+    /// for (d, t) in data.into_iter().online_abs_max().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_abs_max(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -138,7 +251,18 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
-
+    /// Running min.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., -4.];
+    /// let vec_true: Vec<f64> = vec![1., 1., 1., -4.];
+    /// for (d, t) in data.into_iter().online_min().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_min(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -149,6 +273,18 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running peak to peak.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., -4.];
+    /// let vec_true: Vec<f64> = vec![0., 1., 2., 7.];
+    /// for (d, t) in data.into_iter().online_ptp().zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_ptp(self) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -159,6 +295,23 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running quantile.
+    /// # Arguments
+    /// * `q` - Desired quantile.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let vec_true: Vec<f64> = vec![9., 7., 7., 3., 3., 6., 6., 6., 5.];
+    /// let data: Vec<f64> = vec![9., 7., 3., 2., 6., 1., 8., 5., 4.];
+    /// for (d, t) in data
+    ///     .into_iter()
+    ///     .online_quantile(0.5_f64)
+    ///     .zip(vec_true.into_iter())
+    /// {
+    ///     assert_eq!(d, t);
+    /// }
+    /// ```
     fn online_quantile(self, q: Self::Item) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -169,6 +322,20 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running Skewness.
+    /// # Arguments
+    /// * `bias` - If `false`, then the calculations are corrected for statistical bias.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., -4.];
+    /// let vec_true: Vec<f64> = vec![0., 0., 0., -1.5970779829307837];
+    /// for (d, t) in data.into_iter().online_skew(false).zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_skew(self, bias: bool) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -179,6 +346,20 @@ pub trait IterStatExt: Iterator {
             underlying: self,
         }
     }
+    /// Running Variance.
+    /// # Arguments
+    /// * `ddof` - Delta Degrees of Freedom. The divisor used in calculations is `n - ddof`, where `n` represents the number of seen elements.
+    /// # Examples
+    ///
+    /// ```
+    /// use online_statistics::iter::IterStatisticsExtend;
+    /// let data: Vec<f64> = vec![1., 2., 3., -4.];
+    /// let vec_true: Vec<f64> = vec![0., 0.5, 1., 9.666666666666666];
+    /// for (d, t) in data.into_iter().online_var(1).zip(vec_true.into_iter()){
+    ///     assert_eq!(d, t);
+    /// }
+    ///
+    /// ```
     fn online_var(self, ddof: u32) -> IterStat<Self>
     where
         Self::Item: Float + FromPrimitive + AddAssign + SubAssign,
@@ -190,32 +371,4 @@ pub trait IterStatExt: Iterator {
         }
     }
 }
-impl<I: Iterator> IterStatExt for I {}
-
-#[cfg(test)]
-mod tests {
-
-    use super::IterStatExt;
-
-    #[test]
-    fn test_online_sum() {
-        let data: Vec<f64> = vec![1., 2., 3.];
-        let vec_true: Vec<f64> = vec![1., 3., 6.];
-        for (d, t) in data.into_iter().online_sum().zip(vec_true.into_iter()) {
-            assert_eq!(d, t);
-        }
-    }
-    #[test]
-    fn test_online_quantile() {
-        let vec_true: Vec<f64> = vec![9., 7., 7., 3., 3., 6., 6., 6., 5.];
-        let data: Vec<f64> = vec![9., 7., 3., 2., 6., 1., 8., 5., 4.];
-        for (d, t) in data
-            .into_iter()
-            .online_quantile(0.5_f64)
-            .zip(vec_true.into_iter())
-        {
-            println!("{},{}", d, t);
-            assert_eq!(d, t);
-        }
-    }
-}
+impl<I: Iterator> IterStatisticsExtend for I {}
